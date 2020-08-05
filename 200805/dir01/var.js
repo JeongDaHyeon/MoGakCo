26f812720de4857434f243a984695c00b7499b28
JeongDaHyeon <48541114+JeongDaHyeon@users.noreply.github.com>
2020-08-05 16:54:41 +0900

    Create var.js

---
+++ 200805/dir01/var.js
@@ -0,0 +1,11 @@
+// 변수
+const odd = '홀수입니다.';
+const even = '짝수입니다.';
+
+// module.exports에 변수들을 담은 객체를 대입
+// 변수들을 모아둔 모듈로서 기능
+// 다른파일에서 module.exports에 대입된 값을 사용할 수 있음
+module.exports = {
+    odd,
+    even,
+};
